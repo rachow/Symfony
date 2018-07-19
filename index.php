d305eb077d37af3fa10655d3621ee40cd441f0fc
Rahel Ahmed <RahelA@transportinnovation.co.uk>
2018-07-19 11:37:10 +0100

    adding the musical instruments to the mix git bit watever makes sense less

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ *	@author: rchow
+ *	Let's not get Musical about this,
+ *	Simply redirect, on production /web/ directory is the serving root!
+*/
+
+header('Location: /symfony/web/');
+exit;
+
